8f8aab4a38fabc4a23f0c343e32e9275181047a2
Philip <philip0903@hotmail.com>
2024-01-10 11:56:14 -0800

    change background color add new poem object

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },  
+  {
+    author: "philip",
+    poem: "Roses are red \n Good morning \n Good afternoon \n Good night" ,
+    image: "hawaii.jpg"
+  }
 ];
 
 let displayPoems = (array) => {
